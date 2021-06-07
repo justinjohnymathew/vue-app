8f8fad7d1f4638f8c50666c6de5b2170a017ed44
justinjohnymathew <justinjohny.jj@gmail.com>
2021-06-07 23:42:55 +0530

    added Navbar component

---
+++ src/components/Nav/AuthenticatedNavLinks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col -mx-4 md:flex-row md:items-center md:mx-8">
+                        <span @click="logout" class="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Logout</span>
+                    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'AuthNav',
+  methods: {
+    logout () {
+      this.$store.dispatch('logout')
+        .then(() => this.$router.push('/login'))
+    }
+  }
+}
+</script>
---
+++ src/components/Nav/NonAuthenticatedNavLinks.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col -mx-4 md:flex-row md:items-center md:mx-8">
+                        <router-link  to="/login" class="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Sign In</router-link>
+                        <router-link to="/register" class="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Sign Up</router-link>
+                    </div>
+
+</template>
+<script>
+export default {
+  name: 'NonAuthNav'
+}
+</script>
