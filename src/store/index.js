6b92d86fccfed21515612c2c1e5d022d1f4a1b94
justinjohnymathew <justinjohny.jj@gmail.com>
2021-06-08 13:44:08 +0530

    updated vuex store with actions and mutations for theme

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,134 @@
+import { Promise } from 'core-js'
 import { createStore } from 'vuex'
 
 export default createStore({
+
   state: {
+    currentUser: localStorage.getItem('currentUser') || '', // stores details of current user
+    users: JSON.parse(localStorage.getItem('users')) || [], // used to store user details
+    usernames: JSON.parse(localStorage.getItem('usernames')) || [], // used to store usernames
+    status: '',
+    error: '',
+    theme: ''
   },
+
   mutations: {
+    REGISTER (state, user) {
+      // takes the state an the user object as input
+      // updates the user array and the localstorage
+      // sets the current user to the user who registered
+      state.currentuser = user
+      state.users.push(user)
+      state.usernames.push(user.username)
+      localStorage.setItem('users', JSON.stringify(state.users))
+      localStorage.setItem('usernames', JSON.stringify(state.usernames))
+      localStorage.setItem('currentUser', JSON.stringify(user))
+
+      state.status = 'success'
+    },
+
+    LOGIN (state, user) {
+      // updates currntUser state and sets localstorage currentUser to the user who logged in"
+      state.currentUser = user
+      localStorage.setItem('currentUser', JSON.stringify(user))
+    },
+
+    LOGOUT (state) {
+      // clears the currentUser state and clears localstorage
+      state.currentUser = ''
+      localStorage.removeItem('currentUser')
+    },
+
+    SET_THEME (state, theme) {
+      // updates the theme state
+      state.theme = theme
+      localStorage.setItem('theme', theme)
+    }
   },
+
   actions: {
+
+    // action for registration of a new user
+    register ({ commit, state }, user) {
+      return new Promise((resolve, reject) => {
+        if (!state.usernames.includes(user.username)) {
+          // checks wheather username already exist
+          // if not user is succesfully registered
+          commit('REGISTER', user)
+          resolve()
+        } else {
+          // if user already exits then error is returened
+          // commit('register_failure', 'User with this username already exists')
+          const err = new Error('Username Already Exist')
+          reject(err)
+        }
+      })
+    },
+
+    // action to autenticate an existing user
+    login ({ commit, state }, user) {
+      return new Promise((resolve, reject) => {
+        let userId = ''// stores array index of user
+        userId = state.usernames.indexOf(user.username)// returns the index if username in state.usernames
+        if (userId !== -1) {
+          // IF is executed if the username is present in state.usernames
+          if (state.users[userId].password === user.password) {
+            // executed if password is correct
+            commit('LOGIN', state.users[userId])
+            resolve()
+          } else {
+            reject(Error('Incorrect Password'))
+          }
+        } else {
+          reject(Error('Unabale to login using the following credintial'))
+        }
+      })
+    },
+
+    // action for LOGOUT operation
+    logout ({ commit }) {
+      return new Promise((resolve, reject) => {
+        // commits LOGOUTs mutation
+        commit('LOGOUT')
+        resolve()
+      })
+    },
+
+    // action to initialize Theme
+    initTheme ({ commit }) {
+      const cachedTheme = localStorage.theme ? localStorage.theme : false
+      const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+      if (cachedTheme) {
+        // Executed if theme is in localstorage
+        commit('SET_THEME', cachedTheme)
+      } else if (userPrefersDark) {
+        // commits user's device default theme
+        commit('SET_THEME', 'dark')
+      } else {
+        // commit light theme as default
+        commit('SET_THEME', 'light')
+      }
+    },
+    // action for changing theme
+    toggleTheme ({ commit }) {
+      // swith theme from light to datrkand vice versa
+      switch (localStorage.theme) {
+        case 'light':
+          commit('SET_THEME', 'dark')
+          break
+
+        default:
+          commit('SET_THEME', 'light')
+          break
+      }
+    }
+
+  },
+
+  getters: {
+    isLoggedIn: state => !!state.currentUser, // current ststus of the router
+    getTheme: state => state.theme // returns Current Theme
   },
   modules: {
   }
