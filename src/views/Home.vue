493261b9964f59e36651ad5621e1a6a2b5ef4e32
justinjohnymathew <justinjohny.jj@gmail.com>
2021-06-09 17:15:19 +0530

    Added a aHome Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="w-full my-auto flex justify-center items-center">
+        <div class="mx-4 my-32 text-center dark:text-white ">
+            <h1 class="font-bold text-6xl mb-4">Consult a Doctor</h1>
+            <h2 class="font-bold text-3xl mb-12">From Your Home</h2>
+            <div>
+                <router-link to="/login" class="bg-blue-500 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-2">
+                    Sign In
+                </router-link>
+                <router-link to="/register" class="bg-blue-500 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 ml-2">
+                    Sign Up
+                </router-link>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
