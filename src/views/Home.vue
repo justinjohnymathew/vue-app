b796aac22849895d4cdf64f146ad108e7d66b2e7
justinjohnymathew <justinjohny.jj@gmail.com>
2021-06-10 02:18:32 +0530

    Updated Ui of Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <div class="w-full my-auto flex justify-center items-center">
-        <div class="mx-4 my-32 text-center dark:text-white ">
+    <div class="my-auto flex flex-col md:flex-row justify-center items-center ">
+        <div class="w-full p-4">
+        <img src="@/assets/doctor.svg" />
+        </div>
+        <div class="w-full mx-4 my-32 text-center dark:text-white  ">
             <h1 class="font-bold text-6xl mb-4">Consult a Doctor</h1>
             <h2 class="font-bold text-3xl mb-12">From Your Home</h2>
-            <div>
-                <router-link to="/login" class="bg-blue-500 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-2">
+            <div v-show="!isLoggedIn">
+                <router-link to="/login" class="bg-gray-700 hover:bg-gray-600 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-2">
                     Sign In
                 </router-link>
-                <router-link to="/register" class="bg-blue-500 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 ml-2">
+                <router-link to="/register" class="bg-gray-700 hover:bg-gray-600 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 ml-2">
                     Sign Up
                 </router-link>
             </div>
@@ -19,8 +22,13 @@
 
 <script>
 // @ is an alias to /src
-
+import { mapGetters } from 'vuex'
 export default {
-  name: 'Home'
+  name: 'Home',
+  computed: {
+    ...mapGetters([
+      'isLoggedIn'// access loggedin status
+    ])
+  }
 }
 </script>
