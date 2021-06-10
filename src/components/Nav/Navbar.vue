9fc82d203ca5fde7d2fdd09e67b4b7e62125c1f0
justinjohnymathew <justinjohny.jj@gmail.com>
2021-06-10 15:28:43 +0530

    Updated Navbar

--- src/components/Nav/Navbar.vue
+++ src/components/Nav/Navbar.vue
@@ -1,14 +1,15 @@
 <template>
-    <nav class="bg-white shadow border-b-2 border-fuchsia-600 dark:border-grey-700 dark:bg-gray-800">
+    <nav class=" bg-white dark:bg-gray-800 shadow border-b-2 border-fuchsia-600 dark:border-grey-700 dark:bg-gray-800 sticky top-0 z-50">
         <div class="container px-6 py-4 mx-auto">
             <div class="md:flex md:items-center md:justify-between">
                 <div class="flex items-center justify-between">
-                    <div class="text-xl font-semibold text-gray-700">
-                        <a class="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">Book.Doc</a>
+                    <div class="text-xl font-semibold ">
+                        <router-link class="text-2xl font-bold  dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" to="/">Book.Doc</router-link>
                     </div>
 
                     <!-- Mobile menu button -->
                     <div class="flex md:hidden">
+                      <!-- hamburger button for toggling navbar -->
                         <button @click="toggleNavbar()" type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu" >
                             <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
                                 <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
@@ -18,8 +19,8 @@
                 </div>
                 <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
                 <div :class="{ hidden: !navOpened }" class="flex-1 md:flex md:items-center md:justify-end ">
-                    <AuthNav v-if="isLoggedIn" />
-                    <NonAuthNav v-else />
+                    <NonAuthNav v-if="!isLoggedIn" />
+                    <AuthNav v-else />
 
                 </div>
             </div>
@@ -28,8 +29,9 @@
 </template>
 
 <script>
-import NonAuthNav from './NonAuthenticatedNavLinks.vue'
-import AuthNav from './AuthenticatedNavLinks.vue'
+import { mapGetters } from 'vuex'
+import NonAuthNav from './NonAuthenticatedNavLinks.vue'// navbar for non authenticated users
+import AuthNav from './AuthenticatedNavLinks.vue'// navbar for  authenticated users
 export default {
   name: 'Navbar', // component name
   components: {
@@ -37,7 +39,9 @@ export default {
     AuthNav
   },
   computed: {
-    isLoggedIn: function () { return this.$store.getters.isLoggedIn }
+    ...mapGetters([
+      'isLoggedIn'// access loggedin status
+    ])
   },
   data () {
     return {
