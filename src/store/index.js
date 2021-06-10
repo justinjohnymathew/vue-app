c296be8eb2b8f4bf91c4ce925c618f145f90b5ae
justinjohnymathew <justinjohny.jj@gmail.com>
2021-06-10 15:27:39 +0530

    updated vuex-store

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,12 @@ import { createStore } from 'vuex'
 export default createStore({
 
   state: {
-    currentUser: localStorage.getItem('currentUser') || '', // stores details of current user
+    currentUser: localStorage.getItem('currentUser') || '', // { firstname: '', lastname: '', usernmae: '', password: '' }, // stores details of current user
+    user: localStorage.getItem('currentUser') || [],
     users: JSON.parse(localStorage.getItem('users')) || [], // used to store user details
     usernames: JSON.parse(localStorage.getItem('usernames')) || [], // used to store usernames
-    status: '',
+    isAdminUser: localStorage.getItem('isAdminUser') || '',
+    isLoggedin: localStorage.getItem('isLoggedin') || '',
     error: '',
     theme: ''
   },
@@ -18,6 +20,7 @@ export default createStore({
       // updates the user array and the localstorage
       // sets the current user to the user who registered
       state.currentuser = user
+
       state.users.push(user)
       state.usernames.push(user.username)
       localStorage.setItem('users', JSON.stringify(state.users))
@@ -29,14 +32,36 @@ export default createStore({
 
     LOGIN (state, user) {
       // updates currntUser state and sets localstorage currentUser to the user who logged in"
+      // state.currentUser = user
+      state.isLoggedin = 'true'
       state.currentUser = user
-      localStorage.setItem('currentUser', JSON.stringify(user))
+      state.user.push(user)
+      // state.user = Object.assign({}, { firstname: user.firstname, lastname: user.lastname, username: user.username, password: user.password })
+      localStorage.setItem('currentUser', user)
+      localStorage.setItem('isLoggedin', 'true')
+    },
+
+    ADMIN_LOGIN (state) {
+      const admin = { firstname: 'Admin', lastname: 'Admin', usernmae: 'admin', password: 'admin' }
+      state.currentUser = admin
+      // state.currentUser = Object.assign({}, { firstname: admin.firstname, lastname: admin.lastname, username: admin.username, password: admin.password })
+      state.isAdminUser = 'true' // A dirty hack to make isAdminUser true
+      state.isLoggedin = 'true'
+      localStorage.setItem('currentUser', admin)
+      localStorage.setItem('isAdminUser', 'true')
+      localStorage.setItem('isLoggedin', 'true')
     },
 
     LOGOUT (state) {
       // clears the currentUser state and clears localstorage
       state.currentUser = ''
+      state.isAdminUser = ''
+      state.isLoggedIn = ''
+      state.user = []
       localStorage.removeItem('currentUser')
+      localStorage.removeItem('isAdminUser')
+      localStorage.removeItem('isLoggedin')
+      localStorage.removeItem('user')
     },
 
     SET_THEME (state, theme) {
@@ -51,7 +76,7 @@ export default createStore({
     // action for registration of a new user
     register ({ commit, state }, user) {
       return new Promise((resolve, reject) => {
-        if (!state.usernames.includes(user.username)) {
+        if (!state.usernames.includes(user.username) && user.username !== 'admin') {
           // checks wheather username already exist
           // if not user is succesfully registered
           commit('REGISTER', user)
@@ -68,19 +93,25 @@ export default createStore({
     // action to autenticate an existing user
     login ({ commit, state }, user) {
       return new Promise((resolve, reject) => {
-        let userId = ''// stores array index of user
+        console.log('In Login')
+        let userId = -1// stores array index of user
         userId = state.usernames.indexOf(user.username)// returns the index if username in state.usernames
         if (userId !== -1) {
           // IF is executed if the username is present in state.usernames
           if (state.users[userId].password === user.password) {
-            // executed if password is correct
-            commit('LOGIN', state.users[userId])
+            console.log('User')
+            commit('LOGIN', this.state.users[userId])
+            console.log(state.user)
             resolve()
           } else {
-            reject(Error('Incorrect Password'))
+            console.log('error')
+            reject(Error('Unabale to login using the following credintial'))
           }
-        } else {
-          reject(Error('Unabale to login using the following credintial'))
+        } else if (user.username === 'admin' && user.password === 'admin') {
+          // executed if password is correct
+          console.log('Admin')
+          commit('ADMIN_LOGIN')
+          resolve()
         }
       })
     },
@@ -128,7 +159,11 @@ export default createStore({
 
   getters: {
     isLoggedIn: state => !!state.currentUser, // current ststus of the router
-    getTheme: state => state.theme // returns Current Theme
+    getTheme: state => state.theme, // returns Current Theme
+    getUser: state => state.users, // returns users list
+    getCurrentUser: state => state.user, // returns currentuser
+    isAdmin: state => !!state.isAdminUser // tells wheather the user is admin or not
+
   },
   modules: {
   }
